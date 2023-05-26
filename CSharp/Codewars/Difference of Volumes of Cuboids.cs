e8ccfffec6adfed2c35853e56ae98e8b8f804d5c
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-26 16:34:12 +0200

    Codewars | Difference of Volums of Cuboid | C#

---
+++ CSharp/Codewars/Difference of Volumes of Cuboids.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static int FindDifference(int[] a, int[] b)
+  {
+      return Math.Abs(a.Aggregate((x, y) => x * y) - b.Aggregate((x, y) => x * y));
+  }
+
+}
