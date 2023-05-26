684c544d33ab3009894514198c82e1957e17b08d
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-26 17:46:14 +0200

    Leetcode | Recyclable and low fat products | SQL

--- Python/Leetcode/Count odd numbers in an interval range.py
+++ Python/Leetcode/Count odd numbers in an interval range.py
@@ -6,3 +6,17 @@ class Solution:
             count += 1
 
         return count
+
+
+# That was my first solution but it got error - Memory Limit 
+
+# class Solution:
+#     def countOdds(self, low: int, high: int) -> int:
+#         nums = list(range(low, high + 1))
+
+#         count = 0
+#         for i in nums:
+#             if i % 2 != 0:
+#                 count += 1
+
+#         return count
