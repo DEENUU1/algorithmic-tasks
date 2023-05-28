53b63e2c4c3e928d9caa600570fe44da46ea3b17
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-28 15:06:23 +0200

    Leetcode | Lemonade Change | Python

---
+++ Python/Leetcode/Lemonade Change.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten = 0
+        
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if five == 0:
+                    return False
+                five -= 1
+                ten += 1
+            elif bill == 20:
+                if ten > 0 and five > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
