746626bbbaa856c27d819427656528fcbd8beb78
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-05-30 03:14:55 +0200

    Leetcode | Queries Quality and percentage | SQL

---
+++ SQL/Queries QUality and Percentage.sql
@@ -0,0 +1,3 @@
+SELECT query_name, ROUND(SUM(rating/position) / COUNT(query_name), 2) AS quality, ROUND((COUNT(CASE WHEN rating < 3 THEN 1 END) / COUNT(query_name)) * 100, 2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
