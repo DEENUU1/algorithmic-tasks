2b1bced88a98c0f1a00f5de8ccf77f654e1b81f6
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-03 17:25:58 +0200

    Leetcode | Multiply strings | Python

---
+++ Python/Leetcode/Multiply strings.py
@@ -0,0 +1,5 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # Why does this question exist ? 
+        # Complex solution for very simple problem
+        return str(int(num1) * int(num2))
