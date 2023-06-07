a67955b5eb1969ab9fbbee57c199ed8d81abb812
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-06-07 14:15:22 +0200

    Leetcode | Number of 1 bits | Python

---
+++ Python/Leetcode/Number of 1 bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n != 0:
+            res = res + n % 2
+            n = n >> 1
+        
+        return res 
