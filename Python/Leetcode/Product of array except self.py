58cfab569d54f223bad591d5f8d6f0eff1bd67b2
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-08-22 02:30:50 +0200

    Leetcode | Product of array except self | Pyhon

---
+++ Python/Leetcode/Product of array except self.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        res = [1] * (len(nums))
+
+        p = 1
+
+        for i in range(len(nums)):
+            res[i] = p
+            p *= nums[i]
+        
+        pp = 1
+
+        for i in range(len(nums) -1, -1, -1):
+            res[i] *= pp
+            pp *= nums[i]
+
+        return res 
