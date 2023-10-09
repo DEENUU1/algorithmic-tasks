2bd71716e11b6ddcd69c7bd243a033448de6ee90
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-10-09 16:29:30 +0200

    Leetcode | Rotate Image | Python

---
+++ Python/Leetcode/Rotate Image.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r = 0, len(matrix) - 1
+
+        while l < r:
+            for i in range(r - l):
+                t, b = l, r
+                tl = matrix[t][l + i]
+                matrix[t][l + i] = matrix[b - i][l]
+                matrix[b - i][l] = matrix[b][r - i]
+                matrix[b][r - i] = matrix[t + i][r]
+                matrix[t + i][r] = tl
+            r -= 1
+            l += 1
