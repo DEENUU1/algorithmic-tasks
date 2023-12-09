71a515689320eb9a24643221e9da811f11dfd34c
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-09 17:52:35 +0100

    Leetcode | Max number of k sum pairs | Python

--- Python/Leetcode/Max number of k sum pairs.py
+++ Python/Leetcode/Max number of k sum pairs.py
@@ -1,3 +1,6 @@
+
+# Solution 1 - Time error
+
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
 
@@ -12,3 +15,25 @@ class Solution:
         res.reverse()
 
         return len(res)
+
+
+# Solution 2
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+
+        nums.sort()
+        i, j = 0, len(nums) - 1
+        res = 0
+
+        while i < j:
+            s = nums[i] + nums[j]
+            if s == k:
+                i += 1
+                j -= 1
+                res += 1
+            elif s < k:
+                i += 1
+            else:
+                j -= 1
+        return res
