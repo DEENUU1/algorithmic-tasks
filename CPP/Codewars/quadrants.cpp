507359b91c7988c01c672f5071fafd0d188eaf09
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-11 02:08:52 +0100

    Codewars | Quadrans | Cpp

---
+++ CPP/Codewars/quadrants.cpp
@@ -0,0 +1,19 @@
+int quadrant(int x, int y) {
+  if (x > 0) {
+    if (y > 0) {
+      return 1;
+    }
+    else {
+      return 4;
+    }
+  }
+
+  else {
+    if (y > 0) {
+      return 2;
+    }
+    else {
+      return 3;
+    }
+  }
+}
