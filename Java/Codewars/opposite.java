ccdc3ea34656d11e4602460668d2a9ca829888aa
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2024-02-17 23:49:38 +0100

    Create opposite.java

---
+++ Java/Codewars/opposite.java
@@ -0,0 +1,10 @@
+public class Kata
+    {
+        public static int opposite(int number)
+        {
+            if (number > 0){
+              return -number;
+            }
+            return number;
+        }
+    }
