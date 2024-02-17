a0a2babb34e21d918133ff0f17e42a82343041e8
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2024-02-18 00:27:11 +0100

    Create summation.java

---
+++ Java/Codewars/summation.java
@@ -0,0 +1,14 @@
+public class GrassHopper {
+
+    public static int summation(int n) {
+
+        int total = 0;
+      
+        for (int i = 1; i <= n; i++){
+          total += i;
+        }
+      
+        return total;
+      
+    }
+}
