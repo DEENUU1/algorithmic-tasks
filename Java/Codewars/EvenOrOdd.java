b6206ab62cd50ec01f4e307638718c9d70e39762
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2024-02-17 23:34:19 +0100

    Create EvenOrOdd.java

---
+++ Java/Codewars/EvenOrOdd.java
@@ -0,0 +1,9 @@
+public class Kata {
+    public static String evenOrOdd(int number) {     
+        if (number % 2 == 0){
+          return "Even";
+        } else {
+          return "Odd";
+        }
+    }
+}
