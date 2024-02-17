2c2b0685b4de60f98a3041b17b804ac3400779e9
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2024-02-17 23:38:36 +0100

    Create MakeNegative.java

---
+++ Java/Codewars/MakeNegative.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+
+public class Kata {
+
+  public static int makeNegative(final int x) {
+    
+    if (x > 0) {
+      return -x;
+    } else {
+      return x;
+    }
+    
+  }
+  
+}
