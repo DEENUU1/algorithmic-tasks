d5e0e855f8ce73207da3ee9cd99a4142d7c14d90
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2024-03-15 16:16:54 +0100

    Create validPalindrome2.py

---
+++ Python/Leetcode/validPalindrome2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            if s[left] != s[right]:
+                return self.isPalindrome(s, left + 1, right) or self.isPalindrome(s, left, right - 1)
+            left += 1
+            right -= 1
+        
+        return True
+    
+    @staticmethod
+    def isPalindrome(s: str, left: int, right: int) -> bool:
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
