e73191f9835923e0681db40fc5913f20d534ba6c
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2024-03-28 02:15:35 +0100

    Create findMedianSortedArrays.py

---
+++ Python/Leetcode/findMedianSortedArrays.py
@@ -0,0 +1,8 @@
+import statistics
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+
+        return statistics.median(nums1)
